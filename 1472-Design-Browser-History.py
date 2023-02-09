2e92393df8d686fd826b423c51fd466d12a2218b
Anurag Desai <anurag.desai@outlook.com>
2023-02-09 18:26:41 -0500

    1472,1700,225

---
+++ 1472-Design-Browser-History.py
@@ -0,0 +1,74 @@
+class BrowserHistory_DoublyLinkedList:
+    """
+    You have a browser of one tab where you start on the homepage and you can visit another url, 
+    get back in the history number of steps or move forward in the history number of steps.
+
+    Implement the BrowserHistory class:
+
+    BrowserHistory(string homepage) Initializes the object with the homepage of the browser.
+    void visit(string url) Visits url from the current page. It clears up all the forward history.
+    string back(int steps) Move steps back in history. If you can only return x steps in the history and 
+        steps > x, you will return only x steps. Return the current url after moving back in history at most steps.
+    string forward(int steps) Move steps forward in history. If you can only forward x steps in the history and 
+    steps > x, you will forward only x steps. Return the current url after forwarding in history at most steps.
+    """
+    def __init__(self, homepage: str):
+        self.curr = ListNode(homepage)
+
+    def visit(self, url: str) -> None:
+        self.curr.next = ListNode(url, self.curr)
+        self.curr = self.curr.next
+
+    def back(self, steps: int) -> str:
+        while self.curr.prev and steps > 0:
+            self.curr = self.curr.prev
+            steps -= 1
+        return self.curr.val
+
+    def forward(self, steps: int) -> str:
+        while self.curr.next and steps > 0:
+            self.curr = self.curr.next
+            steps -= 1
+        return self.curr.val
+            
+
+class ListNode:
+    def __init__(self, val, prev=None, next=None):
+        self.val = val
+        self.prev = prev
+        self.next = next
+
+        
+# Your BrowserHistory object will be instantiated and called as such:
+# obj = BrowserHistory(homepage)
+# obj.visit(url)
+# param_2 = obj.back(steps)
+# param_3 = obj.forward(steps)
+
+
+class BrowserHistory_Stack:
+    def __init__(self, homepage: str):
+        self.current_position = 0
+        self.len = 1
+        self.history = [homepage]
+
+    def visit(self, url: str) -> None:
+        if len(self.history) < self.current_position + 2:
+            self.history.append(url)
+        else: 
+            self.history[self.current_position + 1] = url
+        self.current_position += 1
+        self.len = self.current_position + 1
+
+        
+    def back(self, steps: int) -> str:
+        # We are using max operation to ensure pointer >= 0
+        # to avoid pointer from going out of lower bounds
+        self.current_position = max(self.current_position - steps, 0)
+        return self.history[self.current_position]
+
+    def forward(self, steps: int) -> str:
+        # We are using max operation to ensure pointer <= 0
+        # to avoid pointer from going out of upper bounds
+        self.current_position = min(self.current_position + steps, self.len - 1)
+        return self.history[self.current_position]
---
+++ 225-Implement-Stack-using-Queues.py
@@ -0,0 +1,31 @@
+class MyStack:
+
+    def __init__(self):
+        self.q = deque()
+
+    def push(self, x: int) -> None:
+        self.q.append(x)
+
+    def pop(self) -> int:
+        # popleft is the same operation as removing from left side 
+        #   so to maintain the logic if we have [1,2,3] and we want to remove "3"
+        #   we will be poping from left and putting the values back till the 
+        #   length of queue. So we will pop "1" and "2" as we normally would and
+        #   append them back in the queue so we would have [3,1,2] and now we will
+        #   pop again and return that value
+        for i in range(len(self.q) - 1):
+            self.push(self.q.popleft())
+        return self.q.popleft()
+
+    def top(self) -> int:
+        return self.q[-1]
+
+    def empty(self) -> bool:
+        return len(self.q) == 0
+
+# Your MyStack object will be instantiated and called as such:
+# obj = MyStack()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.empty()
