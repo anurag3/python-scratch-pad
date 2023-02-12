40ce36fb678235ec2c258a3c450567fc2d1979e6
Anurag Desai <anurag.desai@outlook.com>
2023-02-11 20:19:30 -0500

    Recursion

---
+++ 70-Climbing-Stairs.py
@@ -0,0 +1,16 @@
+class Solution:
+    """
+    You are climbing a staircase. It takes n steps to reach the top.
+
+    Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+    """
+
+    def climbStairs(self, n: int) -> int:
+        if n == 0:
+            return 1
+        elif n == 1:
+            return 1
+        elif n == 2:
+            return 2
+        else:
+            return self.climbStairs(n - 1) + self.climbStairs(n - 2)
